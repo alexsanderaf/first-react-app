5ce0d519d3a487d1b49228ee8b89f62306ba8a1b
alexsanderaf <alexsandera57@gmail.com>
2022-04-24 18:12:52 -0300

    Project upload

---
+++ src/components/PostCard/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const PostCard = ({post}) => {
+    
+    return (
+
+        <div className='post'>
+            <img src={post.cover} alt={post.title} />
+            <div className="post-content">
+                
+                <h2>{post.title} {post.id}</h2>
+                <p>{post.body}</p>
+            </div>
+        </div>
+    )
+}
